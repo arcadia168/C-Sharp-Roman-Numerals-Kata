a297850baefeec3e3b33fc84d47c2ff0c2fd60e2
Erin Eland <erin@erineland.com>
2018-11-04 19:09:55 +0000

    Committing all files to show them on GitHub as a personal development portfolio.

--- BenNawazRomanNumerals/Program.cs
+++ BenNawazRomanNumerals/Program.cs
@@ -6,10 +6,119 @@ using System.Threading.Tasks;
 
 namespace ***REMOVED******REMOVED***RomanNumerals
 {
+    public interface IRomanNumeralGenerator
+    {
+        String generate(int number);
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\nWelcome to the Roman Numeral generator! (Type 'exit' to close program)");
+
+            //Loop and leave the program running until the user exits
+            while (1 != 0)
+            {
+                try
+                {
+                    promptUserForInput();
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+            }
+        }
+
+        private static void promptUserForInput()
+        {
+            Console.Write("\nEnter an positive integer number between 1 and 3999 to recieve it's roman numeral equivalent: ");
+
+            string userInputString = Console.ReadLine();
+
+            if (userInputString.Equals("exit")) 
+            {
+                Console.WriteLine("Goodbye, and thanks for all the fish!");
+                Environment.Exit(0);
+            }
+
+            int numValue;
+            bool parsed = Int32.TryParse(userInputString, out numValue);
+
+            if (!parsed)
+            {
+                throw new ArgumentException("Int32.TryParse could not parse '{0}' to an int.\n", userInputString);
+            }
+            else
+            {
+                //User input was successfully parsed, try and generate a roman numeral from it.
+                RomanNumeralGenerator romanNumeralGenerater = new RomanNumeralGenerator();
+
+
+                String generatedRomanNumeral = romanNumeralGenerater.generate(numValue);
+                Console.WriteLine("Your input number {0} has the corresponding roman numeral: '{1}'! Cool!", userInputString, generatedRomanNumeral);
+            }
+        }
+    }
+
+    //Define our class to generate Roman numerals, which implements the given interface
+    public class RomanNumeralGenerator : IRomanNumeralGenerator
+    {
+        public readonly Dictionary<int, string> RomanNumeralDictionary;
+
+        public RomanNumeralGenerator()
+        {
+            //Dictionary/map to represent all unique roman numeral characters.
+            RomanNumeralDictionary = new Dictionary<int, string>
+                {
+                    { 1000, "M" },
+                    { 900, "CM" },
+                    { 500, "D" },
+                    { 400, "CD" },
+                    { 100, "C" },
+                    { 90, "XC" },
+                    { 50, "L" },
+                    { 40, "XL" },
+                    { 10, "X" },
+                    { 9, "IX" },
+                    { 5, "V" },
+                    { 4, "IV" },
+                    { 1, "I" },
+                };
+        }
+
+        /* APPROACH:
+         * Loop continually, subtracting from our number and appending this value to our roman numeral string.
+         */
+        public String generate(int number)
+        {
+            //Run validation checks on the number - only accept between 1 and 3999
+            if (number < 1 || number > 3999)
+            {
+                throw new ArgumentException("Invalid input, please enter a number between 1 and 3999");
+            }
+
+            //Instantiate a new .NET string builder
+            var roman = new StringBuilder();
+
+            // Iterate over the set of possible roman numeral characters
+            // Must iterate in descending order of value, starting with M = 1000
+            foreach (var romanNumeralChar in this.RomanNumeralDictionary)
+            {
+                //Append the current 'item'
+                while (number >= romanNumeralChar.Key)
+                {
+                    //Append the roman numeral char to our roman numeral string
+                    roman.Append(romanNumeralChar.Value);
+
+                    //Now adjust the number for next iteration
+                    number -= romanNumeralChar.Key;
+                }
+            }
+
+            //Return the fully built roman numeral string.
+            return roman.ToString();
         }
     }
 }
---
+++ BenNawazRomanNumerals/UnitTestProject1/RomanNumeralsTest.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ***REMOVED******REMOVED***RomanNumerals;
+
+namespace ***REMOVED******REMOVED***RomanNumeralGeneratorTests
+{
+    [TestClass]
+    public class GenerateRomanNumeralsTests
+    {
+        static RomanNumeralGenerator testRomanNumeralGeneratorNumeralGenerator = new RomanNumeralGenerator();
+
+        [TestMethod]
+        public void RomanNumeralGenerator_1_I()
+        {
+            //testRomanNumeralGeneratorNumeralGenerator = new RomanNumeralGenerator();
+            Assert.AreEqual("I", testRomanNumeralGeneratorNumeralGenerator.generate(1));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_2_II()
+        {
+            //testRomanNumeralGeneratorNumeralGenerator = new RomanNumeralGenerator();
+            Assert.AreEqual("II", testRomanNumeralGeneratorNumeralGenerator.generate(2));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_3_III()
+        {
+            Assert.AreEqual("III", testRomanNumeralGeneratorNumeralGenerator.generate(3));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_4_IV()
+        {
+            Assert.AreEqual("IV", testRomanNumeralGeneratorNumeralGenerator.generate(4));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_5_V()
+        {
+            Assert.AreEqual("V", testRomanNumeralGeneratorNumeralGenerator.generate(5));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_9_IX()
+        {
+            Assert.AreEqual("IX", testRomanNumeralGeneratorNumeralGenerator.generate(9));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_10_X()
+        {
+            Assert.AreEqual("X", testRomanNumeralGeneratorNumeralGenerator.generate(10));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_49_XLIX()
+        {
+            Assert.AreEqual("XLIX", testRomanNumeralGeneratorNumeralGenerator.generate(49));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_50_L()
+        {
+            Assert.AreEqual("L", testRomanNumeralGeneratorNumeralGenerator.generate(50));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_90_XC()
+        {
+            Assert.AreEqual("XC", testRomanNumeralGeneratorNumeralGenerator.generate(90));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_99_XCIX()
+        {
+            Assert.AreEqual("XCIX", testRomanNumeralGeneratorNumeralGenerator.generate(99));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_100_C()
+        {
+            Assert.AreEqual("C", testRomanNumeralGeneratorNumeralGenerator.generate(100));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_400_CD()
+        {
+            Assert.AreEqual("CD", testRomanNumeralGeneratorNumeralGenerator.generate(400));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_500_D()
+        {
+            Assert.AreEqual("D", testRomanNumeralGeneratorNumeralGenerator.generate(500));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_900_CM()
+        {
+            Assert.AreEqual("CM", testRomanNumeralGeneratorNumeralGenerator.generate(900));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_1000_M()
+        {
+            Assert.AreEqual("M", testRomanNumeralGeneratorNumeralGenerator.generate(1000));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_3999_MMMCMXCIX()
+        {
+            Assert.AreEqual("MMMCMXCIX", testRomanNumeralGeneratorNumeralGenerator.generate(3999));
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_4000_MMMM()
+        {
+            Exception expectedExcetpion = null;
+
+            // Act
+            try
+            {
+                testRomanNumeralGeneratorNumeralGenerator.generate(4000);
+            }
+            catch (Exception ex)
+            {
+                expectedExcetpion = ex;
+            }
+
+            // Assert
+            Assert.IsNotNull(expectedExcetpion);
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_0()
+        {
+            Exception expectedExcetpion = null;
+
+            // Act
+            try
+            {
+                testRomanNumeralGeneratorNumeralGenerator.generate(0);
+            }
+            catch (Exception ex)
+            {
+                expectedExcetpion = ex;
+            }
+
+            // Assert
+            Assert.IsNotNull(expectedExcetpion);
+        }
+
+        [TestMethod]
+        public void RomanNumeralGenerator_minus1()
+        {
+            Exception expectedExcetpion = null;
+
+            // Act
+            try
+            {
+                testRomanNumeralGeneratorNumeralGenerator.generate(-1);
+            }
+            catch (Exception ex)
+            {
+                expectedExcetpion = ex;
+            }
+
+            // Assert
+            Assert.IsNotNull(expectedExcetpion);
+        }
+    }
+}
